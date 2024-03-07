494721a53e0bc3d0e132f92aced90db4231f9155
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 15:15:53 -0600

    Create file

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -0,0 +1,18 @@
+        }
+            }
+                r = mid-1;
+            else{
+            }
+                l = mid+1;
+            else if(target> matrix[mid][cols-1]){
+            }
+                break;
+                finalRow = mid;
+        System.out.println(finalRow);
+        int l2=0, r2= cols-1;
+        while(l2<=r2){
+            int mid= l2+(r2-l2)/2;
+            if(matrix[finalRow][mid]==target){
+                return true;
+            }
+[
