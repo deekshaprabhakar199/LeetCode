1211367d2ae26ab33afd121c18857161bdac3299
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 12:53:48 -0600

    Create file

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -0,0 +1,14 @@
+    public int performOperation(int num1, int num2, String operator){
+        switch(operator){
+            case "+": return num1+ num2;
+            case "-": return num1-num2;
+            case "*": return num1*num2;
+            case "/": return num2/num1;
+        }
+        return 0;
+    }
+}
+
+
+
+[
