4fe75ffc3304fdfb23a77fb06abcecb4cf010122
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 00:43:40 -0600

    Create file

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,14 @@
+        }
+            }
+                if(curr != '.'){
+                char curr = board[i][j];
+                    if(!position.add("Row"+i+curr) || !position.add("Col"+j+curr) || !position.add("Grid"+i/3+j/3+curr)){
+                }
+                        return false;
+                    }
+        return true;
+    }
+}
+            for(int j=0; j< board[0].length;j++){
+        for(int i=0; i< board.length;i++){
+[
