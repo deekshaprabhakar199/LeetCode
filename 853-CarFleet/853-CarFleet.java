d052e9d961e608bb27344a56f7d7a477cc33f519
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 14:42:15 -0600

    Create file

---
+++ 853-CarFleet/853-CarFleet.java
@@ -0,0 +1,17 @@
+            time[position[i]] = (double)(target- position[i]) / speed[i]; 
+        }
+        double previous = 0.0;
+        for(int i= time.length-1; i>=0; i--){
+            double curr= time[i];
+        }
+            if(curr>previous){
+                previous = curr;
+            }
+                res++;
+        return res;
+
+        for( int i=0; i<position.length; i++){
+        double[] time = new double[target];
+        int res=0;
+    }
+1
