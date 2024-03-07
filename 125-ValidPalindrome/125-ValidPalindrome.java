8a5e8323d6b264f028fee28423b57497ceb8c7a0
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 02:44:10 -0600

    Create file

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,18 @@
+                    return false;
+                if(str[l]!=str[r]){
+            else{
+            }
+                r--;
+            else if(!isAlphanumeric(str[r])){
+            }
+            if(!isAlphanumeric(str[l])){
+                l++;
+        while(l<=r){
+        int r= s.length()-1;
+                }
+                l++;
+                r--;
+            }
+        }
+        return true;
+"
