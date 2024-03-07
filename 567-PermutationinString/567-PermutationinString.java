fa11628f38f0340709f60fb821a6bea94e060836
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 12:23:48 -0600

    Create file

---
+++ 567-PermutationinString/567-PermutationinString.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] str1 = new int[26];
+        int[] str2 = new int[26];
+
+        for( int i=0; i< l1;i++){
+            str1[s1.charAt(i)-'a'] ++;
+        }
+            str2[s2.charAt(i)-'a']++;
+        if(Arrays.equals(str1,str2)) return true;
+        for( int i = l1; i< l2; i++){
+            str2[s2.charAt(i-l1)-'a']--;
+            str2[s2.charAt(i)-'a']++;
+        int l1 = s1.length();
+        int l2 = s2.length();
+"
