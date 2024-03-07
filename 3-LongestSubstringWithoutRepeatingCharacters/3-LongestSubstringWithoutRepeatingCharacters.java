f64be3eef35e94fbf29ca99667d2a12a205ad9d8
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 10:28:39 -0600

    Create file

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,14 @@
+            while(unique.contains(str[r])){
+                maxLength= Math.max(maxLength, unique.size());
+            }
+                unique.remove(str[l]);
+                l++;
+        while(r<str.length){
+        HashSet<Character> unique = new HashSet<>(); 
+        int maxLength=Integer.MIN_VALUE;
+        char[] str = s.toCharArray();
+        int r=0;
+        int l =0;
+    public int lengthOfLongestSubstring(String s) {
+class Solution {
+"
