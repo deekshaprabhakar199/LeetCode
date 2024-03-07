c47df2ae0acb5db736cdd7c303499ed39fd4d7b6
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 12:24:57 -0600

    Update file 3/7/2024, 12:24:57 PM

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -9,8 +9,8 @@ class Solution {
             str2[s2.charAt(i)-'a']++;
         if(Arrays.equals(str1,str2)) return true;
         for( int i = l1; i< l2; i++){
-            str2[s2.charAt(i-l1)-'a']--;
-            str2[s2.charAt(i)-'a']++;
         int l1 = s1.length();
         int l2 = s2.length();
+        if(s1.length()==0) return true;
+        if(s1.length() > s2.length()) return false;
 "
