37cb69d0b92d822631957864a94530b9cd8a91e2
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 13:10:40 -0600

    Create file

---
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -0,0 +1,12 @@
+        }
+            index.push(i);
+            }
+                result[previous] = i- previous;
+                int previous = index.pop();
+        for(int i=0; i<temperatures.length; i++){
+            while(!index.isEmpty() && temperatures[i] > temperatures[index.peek()]){
+        return result;
+    }
+}
+
+[
