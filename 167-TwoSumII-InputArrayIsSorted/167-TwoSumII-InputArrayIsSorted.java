003e45f244707c525833b5cc6bab297b8e40543f
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 02:47:42 -0600

    Create file

---
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -0,0 +1,16 @@
+        }
+                r--;
+            }
+            else if( sum< target){
+                l++;
+            }
+            else{
+                return new int[]{l+1,r+1};
+            }
+            if(sum>target){
+            int sum= nums[l]+nums[r];
+        while(l<=r){
+        return new int[]{0,0};
+    }
+}
+[
