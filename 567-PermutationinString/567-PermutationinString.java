7527b9cb6f965dca2f4382e06dbc068c2cb20a37
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-07 12:26:05 -0600

    Update file 3/7/2024, 12:26:05 PM

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,16 +1,15 @@
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int[] str1 = new int[26];
-        int[] str2 = new int[26];
-
-        for( int i=0; i< l1;i++){
-            str1[s1.charAt(i)-'a'] ++;
         }
             str2[s2.charAt(i)-'a']++;
         if(Arrays.equals(str1,str2)) return true;
         for( int i = l1; i< l2; i++){
-        int l1 = s1.length();
-        int l2 = s2.length();
-        if(s1.length()==0) return true;
-        if(s1.length() > s2.length()) return false;
+            str2[s2.charAt(i-l1)-'a']--;
+            str2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(str1,str2)) return true;
+        }
+        return false;
+    }
+}
+
+//Time = O(s2.length())
+//spcae = O(1)
 "
