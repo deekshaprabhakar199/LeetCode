5c513199f345746df9023c1c9736ccbe0a9e80c2
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-08 07:51:58 -0600

    Create file

---
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -0,0 +1,17 @@
+            return 0;
+        }
+
+        int leftDepth = calculateDepth(root.left);
+        int rightDepth = calculateDepth(root.right);
+
+         diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        if (root == null) {
+    private int calculateDepth(TreeNode root) {
+
+    }
+        return diameter;
+        calculateDepth(root);
+    public int diameterOfBinaryTree(TreeNode root) {
+
+[
