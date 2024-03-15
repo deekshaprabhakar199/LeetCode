a02e3b4b3736bd489ba532ff89dbf7fc16d0ac03
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-14 21:08:35 -0500

    Update file 3/14/2024, 9:08:35 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,15 +1,14 @@
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        for(int i: nums){
-            freqMap.put(i, freqMap.getOrDefault(i,0)+1);
+                    }
+                }
+            }
         }
-        ArrayList<Integer> freq= new ArrayList<>(freqMap.keySet());
-        freq.sort((a,b)-> freqMap.get(b)- freqMap.get(a));
-        for(int i=0; i<k; i++){
-            ans[i]=freq.get(i);
-        }
-    }
-        int[] ans= new int[k];
-        return ans;
+        
+                        break;
+                    if (index == k) {
+                    result[index++] = num;
+                for (int num : buckets[i]) {
+            if (buckets[i] != null) {
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+        int index = 0;
+        int[] result = new int[k];
 [
