05a36c30c6e5887690d613f5e8bc44bb06e8e4c8
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-17 13:37:18 -0500

    Update file 3/17/2024, 1:37:18 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,14 +1,11 @@
-            else if(nums[mid]>target && nums[mid]>nums[nums.length-1]){
-                l = mid+1;
+                }
+                    r = mid-1;
+                else{
+                }
+                    l = mid+1;
+                if(nums[mid]<target && target <= nums[nums.length-1]){
+            if(nums[mid]< nums[nums.length-1]){
             }
-            else if(nums[mid]>target && nums[mid]<nums[nums.length-1]){
-                r = mid-1;
-            }
-            else {
-                r = mid-1;
-            }
-        }
-        return -1;
-    }
-}
+            if(nums[mid]==target){
+                return mid;
 [
