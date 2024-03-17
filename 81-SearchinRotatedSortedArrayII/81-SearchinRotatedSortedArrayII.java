347ce5743288dd90508f0c7136a0d97c145af4e5
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-17 14:06:45 -0500

    Create file

---
+++ 81-SearchinRotatedSortedArrayII/81-SearchinRotatedSortedArrayII.java
@@ -0,0 +1,22 @@
+            }
+                }
+                    r = mid-1;
+                else{
+                }
+                    l = mid+1;
+                if(nums[mid]<target && target <= nums[nums.length-1]){
+            }
+            else if(nums[mid] <= nums[nums.length-1]){
+                r = r-1;
+                continue;
+                return true;
+            }
+            else if(nums[l]== nums[mid] && nums[mid]==nums[r]){
+                l = l+1;
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            if(nums[mid]==target){
+    public boolean search(int[] nums, int target) {
+        int l=0, r= nums.length-1;
+class Solution {
+[
