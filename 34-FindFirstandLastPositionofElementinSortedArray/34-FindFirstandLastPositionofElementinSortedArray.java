f3537f42a8414aea32f6ed17cbe7f569c9f26fee
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-17 12:44:20 -0500

    Update file 3/17/2024, 12:44:19 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,16 +1,16 @@
             }
-                l = mid+1;
-            }
-            else if(nums[mid]< target){
-                r=mid-1;
             if(nums[mid]>target){
+                r=mid-1;
             int mid= l+(r-l)/2;
-        while(l<=r){
         int l=0, r= nums.length-1, ans=-1;
+        while(l<=r){
     public int search(int[] nums, int target, boolean isFirst){
 
     }
-    public int[] searchRange(int[] nums, int target) {
         return new int[]{search(nums, target, true),search(nums, target, false)};
+    public int[] searchRange(int[] nums, int target) {
 class Solution {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
 [
