5ca66db5558efc8f980a1e2a0b18f9b43ee167c0
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-03-17 20:41:44 -0500

    Create file

---
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.java
@@ -0,0 +1,18 @@
+                    r=mid-1;
+                }
+            }
+            else{
+                else{
+                }
+                    l = mid+1;
+                if(mid>0 && nums[mid-1]==nums[mid]){
+            if(mid%2 != 0){
+            }
+                    return nums[mid];
+            if(mid>0 && mid<nums.length-1 && nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+            mid = l+(r-l)/2;
+                if(mid >0 && nums[mid-1]==nums[mid]){
+                    r = mid-2;
+                }
+                else{
+[
