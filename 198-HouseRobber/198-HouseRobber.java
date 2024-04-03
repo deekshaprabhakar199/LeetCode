f68dde65492fc1565d1981e9a49c65dde1abbb69
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-03 12:21:34 -0500

    Create file

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,17 @@
+    public int robHelper(int n, int[] ans, int[] nums){
+    
+    }
+        return robHelper(n-1,ans, nums);
+        Arrays.fill(ans,-1);
+        int[] ans = new int[n];
+        int n= nums.length;
+        // return money[money.length-1];
+        // }
+        //     money[i]= Math.max(money[i-1], money[i-2]+nums[i]);
+        // for(int i=2; i<nums.length ;i++){
+
+        // money[1]= Math.max(nums[0],nums[1]);
+        // money[0]= nums[0];
+
+        // int[] money = new int[nums.length];
+[
