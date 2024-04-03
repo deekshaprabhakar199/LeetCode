0b0636fa8795df43907ab9a338f938e0c589f278
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-03 12:31:32 -0500

    Update file 4/3/2024, 12:31:32 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,10 +1,16 @@
-
-        //space optimization
+        if(n<2) return nums[0];
         int prev = nums[0];
         int next = Math.max(prev, nums[1]);
         for( int i= 2; i<n; i++ ){
-        // return robHelper(n-1,ans, nums);
-        // Arrays.fill(ans,-1);
-        // int[] ans = new int[n];
-        if(n<2) return nums[0];
+            int curr = Math.max(prev+ nums[i], next);
+            prev = next;
+            next = curr;
+        }
+        return next;
+    }
+    
+}
+class Solution {
+    public int rob(int[] nums) {
+        int n= nums.length;
 [
