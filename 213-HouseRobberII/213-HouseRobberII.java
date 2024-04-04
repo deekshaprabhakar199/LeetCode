0f0d27be69b770fab17527911a88364402ec1404
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-04 11:47:06 -0500

    Update file 4/4/2024, 11:47:06 AM

--- 213-HouseRobberII/213-HouseRobberII.java
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -1,14 +1,16 @@
         int prev = nums[start];  
         int next = Math.max(nums[start], nums[start+1]);
         for( int i=start+2; i<=n; i++){
-            int curr= Math.max(nums[i-2]+nums[i], nums[i-1]);
+            int curr= Math.max(prev+nums[i], next);
             prev = next;
-    public int robHelper(int start, int n, int[] nums){
-
             next = curr;
         }
         return next;
         
     }
 }
+    public int robHelper(int start, int n, int[] nums){
+
+    }
+        return Math.max(rob1, rob2);
 [
