3c00b1583a27207fd28933e4cf2b9897ee9f3243
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-07 18:25:33 -0500

    Update file 4/7/2024, 6:25:33 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,12 +1,10 @@
-        HashSet<Integer> unique = new HashSet<>();
-        for( int i: nums){
-            if(!unique.add(i)) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> unique = new HashSet<>();
+        for(int i: nums){
+            if(!unique.add(i)) return true;
+        }
+    }
+        return false;
+}
 [
