d8c9f7f699c0e47aee68be19e63c640265693342
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-07 19:11:07 -0500

    Create file

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        for(String str: strs){
+            char[] s1 = str.toCharArray();
+        }
+    }
+            Arrays.sort(s1);
+            String s2 = new String(s1);
+            anagram.putIfAbsent(s2, new ArrayList<String>());
+        HashMap<String, List<String>> anagram = new HashMap<>();
+        return new ArrayList<>(anagram.values());
+            anagram.get(s2).add(str);
+}
+[
