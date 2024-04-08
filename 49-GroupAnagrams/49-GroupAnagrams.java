e08bb7c16b44f048ad85cc46f063fcd768fb1cf0
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-07 23:26:54 -0500

    Update file 4/7/2024, 11:26:54 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,14 +1,18 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        for(String str: strs){
-            char[] s1 = str.toCharArray();
         }
-    }
-            Arrays.sort(s1);
-            String s2 = new String(s1);
-            anagram.putIfAbsent(s2, new ArrayList<String>());
-        HashMap<String, List<String>> anagram = new HashMap<>();
-        return new ArrayList<>(anagram.values());
-            anagram.get(s2).add(str);
-}
+            }
+            bucket[frequency].add(i);
+
+        for(int i= bucket.length-1; i>=0 ;i--){
+            if(bucket[i] != null){
+                for(int num:bucket[i]){
+                    result[index] = num;
+                    index++;
+                    if(index == k){
+                        return result;
+                    }
+                }
+            }
+        }
+                bucket[frequency] = new ArrayList<Integer>();
+            if (bucket[frequency] == null){
 [
