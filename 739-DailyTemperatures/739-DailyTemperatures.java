75eaeed32f0abf1580f29feb262ccf901e0fda28
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 18:32:33 -0500

    Update file 4/8/2024, 6:32:33 PM

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,12 +1,15 @@
+            while(!temp.isEmpty() && temperatures[i] > temperatures[temp.peek()]){
         }
-            index.push(i);
+                ans[temp.peek()] = i-temp.pop();
             }
-                result[previous] = i- previous;
-                int previous = index.pop();
-        for(int i=0; i<temperatures.length; i++){
-            while(!index.isEmpty() && temperatures[i] > temperatures[index.peek()]){
-        return result;
-    }
-}
-
+            temp.push(i);
+        while(!temp.isEmpty()){
+            ans[temp.pop()]=0;
+        }
+        return ans;
+        for( int i=0; i< temperatures.length; i++){
+        int[] ans = new int[temperatures.length];
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> temp = new Stack<>();
+class Solution {
 [
