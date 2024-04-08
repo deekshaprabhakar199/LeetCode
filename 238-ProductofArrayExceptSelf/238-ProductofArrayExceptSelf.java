72f78debfe62d4f993e88c1e3fdcac2eec43183e
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-07 23:34:45 -0500

    Update file 4/7/2024, 11:34:44 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,12 +1,15 @@
             ans[i] = ans[i-1] * nums[i-1];
         }
-        int suffix = 1;
-        for( int i = nums.length-1; i>=0; i--){
-            ans[i] = suffix * ans[i];
-            suffix = suffix * nums[i];
+        int multiplier = 1;
+        for( int i=nums.length-1; i>=0; i--){
+            ans[i] = ans[i] * multiplier;
+        }
+    }
+            multiplier = multiplier * nums[i];
+        return ans;
         for( int i=1; i< nums.length; i++){
-        ans[0]=1;
-    public int[] productExceptSelf(int[] nums) {
+        ans[0] = 1;
         int[] ans = new int[nums.length];
-class Solution {
+    public int[] productExceptSelf(int[] nums) {
+}
 [
