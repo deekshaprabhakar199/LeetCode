8506b4b6cc314ab13ffc43b4bcbc62656503e7ee
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 00:00:28 -0500

    Update file 4/8/2024, 12:00:27 AM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -1,22 +1,16 @@
-        int currLength=0;
-        for( int i: nums){
-            unique.add(i);
-        }
-        for( int i: nums){
-            if(!unique.contains(i-1)){
-        }
-        int maxLength=0;
                 currLength=1;
             }
                 while(unique.contains(i+1)){
                     currLength++;
                 }
-    }
+        }
                     i++;
                 maxLength = Math.max(maxLength, currLength);
-        return maxLength;
-}
+        for( int i: nums){
+            if(!unique.contains(i-1)){
+        }
+            unique.add(i);
+        for( int i: nums){
+        int currLength = 0, maxLength= 0;
         HashSet<Integer> unique = new HashSet<>();
-
-//Time = Spacce = O(n)
 [
