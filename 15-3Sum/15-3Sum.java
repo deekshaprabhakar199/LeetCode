0cb6990e0d03ce1e0d70dc96f4888a226df29f12
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 13:24:51 -0500

    Create file

---
+++ 15-3Sum/15-3Sum.java
@@ -0,0 +1,17 @@
+                int remainder = target - nums[j];
+                if(nums[k] > remainder){
+                    k--;
+                }
+            int j=i+1;
+            int k= nums.length-1;
+            while(j<k){
+                continue;
+            }
+            int target = -nums[i];
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplet = new ArrayList<>();
+        Arrays.sort(nums);
+        for( int i=0; i< nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1]){
+class Solution {
+[
