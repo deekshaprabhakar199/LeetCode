d04ecebf87dbb4f598c0f356fc1f0a5904da7a64
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 13:45:46 -0500

    Update file 4/8/2024, 1:45:46 PM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -2,14 +2,14 @@
         }
                 case '(' : parenthesis.push(')'); break;
             }
-    }
                 case '{' : parenthesis.push('}'); break;
                 case '[' : parenthesis.push(']'); break;
                 case ')' : case ']' : case '}' : if(parenthesis.isEmpty() || parenthesis.pop() != s.charAt(i)) return false;
-        return (parenthesis.isEmpty());
-}
         for( int i=0; i< s.length() ; i++){
         Stack<Character> parenthesis = new Stack<>();
 class Solution {
     public boolean isValid(String s) {
+        if(s.length() == 1){
+            return false;
+        }
 "
