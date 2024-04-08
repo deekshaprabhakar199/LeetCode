18f90a7bb2c7f81198eae9cbaa5116915210a4a4
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-07 23:47:39 -0500

    Update file 4/7/2024, 11:47:38 PM

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,14 +1,15 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for( int i=0; i< board.length; i++){
+            for(int j=0; j< board[0].length; j++){
         }
             }
-                if(curr != '.'){
-                char curr = board[i][j];
-                    if(!position.add("Row"+i+curr) || !position.add("Col"+j+curr) || !position.add("Grid"+i/3+j/3+curr)){
+        HashSet<String> unique = new HashSet<>();
+        return true;
+                if(board[i][j] != '.'){
+                    if(!unique.add("Row"+i+board[i][j]) || !unique.add("Col"+j+board[i][j]) || !unique.add("Grid"+i/3+j/3+board[i][j])){
                 }
                         return false;
                     }
-        return true;
     }
-}
-            for(int j=0; j< board[0].length;j++){
-        for(int i=0; i< board.length;i++){
 [
