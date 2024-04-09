16ff71559a1b13b6a42807bf26259e8d7de08a01
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 16:32:00 -0500

    Create file

---
+++ 90-SubsetsII/90-SubsetsII.java
@@ -0,0 +1,16 @@
+    }
+        return result;
+
+    public void backtrack(List<Integer> temp, int[] nums, int start){
+        result.add(new ArrayList<>(temp));
+        for( int i=start; i< nums.length; i++){
+            if(i>start && nums[i]==nums[i-1]){
+        backtrack(new ArrayList<>(), nums, 0);
+        Arrays.sort(nums);
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+
+    List<List<Integer>> result = new ArrayList<>();
+                return;
+            }
+[
