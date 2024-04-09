5a91a86053915e067a2169ae2068fe0faaea7439
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 00:28:43 -0500

    Update file 4/9/2024, 12:28:43 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,3 +1,10 @@
+        ListNode prev = new ListNode(0, null);
+    public ListNode reverseList(ListNode head) {
+class Solution {
+        ListNode curr= head;
+        ListNode temp = new ListNode(0);
+        while(curr != null){
+            temp.next = curr.next;
             curr.next = prev.next;
             prev.next = curr;
             curr= temp.next;
