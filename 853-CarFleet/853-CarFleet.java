39e940feeb37a8bfd31600832ec957771d52b17a
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 19:16:27 -0500

    Update file 4/8/2024, 7:16:27 PM

--- 853-CarFleet/853-CarFleet.java
+++ 853-CarFleet/853-CarFleet.java
@@ -1,16 +1,25 @@
-        for( int i=0; i<position.length; i++){
-            time[position[i]] = (double)(target- position[i]) / speed[i]; 
-        }
-        for(int i = time.length-1; i>=0; i--){
-            double curr = time[i];
-        }
-        double prev = 0.0;
-            if(curr > prev){
-                prev = curr;
-            }
+    public Set<String> helper(int n,Set<String> dp[]){
     }
-                fleets++;
-        return fleets;
-}
-//time[position[i]] is the asn to optimisation
-1
+        return res; 
+       
+        Set<String> dp[] = new HashSet[n+1];
+        List<String> res = new ArrayList<>(helper(n,dp));
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+
+
+        if(n==1){
+            Set<String> res = new HashSet<>();
+            res.add("()");
+            return res;
+        }
+        if(dp[n]!=null)
+            return dp[n];
+
+        Set<String> res = new HashSet<>();
+        for(int i=1;i<n;i++){
+            Set<String> left = helper(i,dp);
+            Set<String> right = helper(n-i,dp);
+            if(i==1){
+3
