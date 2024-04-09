278b275cddbebc5fcd69c9e7c30c21822c83c436
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 00:02:27 -0500

    Update file 4/9/2024, 12:02:27 AM

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,9 +1,18 @@
         }
             str2[s2.charAt(i)-'a']++;
         if(Arrays.equals(str1,str2)) return true;
-        for( int i = l1; i< l2; i++){
+        for(int i=l1; i<l2; i++){
             str2[s2.charAt(i-l1)-'a']--;
             str2[s2.charAt(i)-'a']++;
+            str1[s1.charAt(i)-'a']++;
+        for( int i=0; i<l1; i++){
+        if(s1.length() > s2.length()) return false;
+        int l1= s1.length(), l2= s2.length();
+        if(s1.length() ==0) return true;
+        int[] str2 = new int[26];
+        int[] str1 = new int[26];
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
             if(Arrays.equals(str1,str2)) return true;
         }
         return false;
