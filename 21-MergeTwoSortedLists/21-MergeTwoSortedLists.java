91c956047aefb77563ce5457cfe5c31564622e01
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 00:37:44 -0500

    Update file 4/9/2024, 12:37:44 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,7 +1,11 @@
+            curr.next = list1;
+            list1 = list1.next;
+        }
+        while(list2 != null){
             curr.next = list2;
             list2=list2.next;
+            curr=curr.next;
         }
         return head.next;
-    }
-}
+            curr=curr.next;
 [
