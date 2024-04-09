a5a4c028a206e9de05b3feec557f8e3a36503cbc
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 10:19:46 -0500

    Create file

---
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).java
@@ -0,0 +1,23 @@
+    public boolean startsWith(String prefix) {
+    
+    }
+        return curr.isWord;
+        }
+            curr = curr.child.get(c);
+            if(!curr.child.containsKey(c)) return false;
+        for(char c: word.toCharArray()){
+        TrieNode curr = root;
+    public boolean search(String word) {
+        curr.isWord=true;
+    }
+    
+        }
+            curr=curr.child.get(c);
+            curr.child.putIfAbsent(c, new TrieNode());
+        for(char c: word.toCharArray()){
+        TrieNode curr = root;
+    public void insert(String word) {
+    
+        TrieNode curr = root;
+        for(char c: prefix.toCharArray()){
+[
