bec36691f300509c1e7c188da22bcc5b202dca9f
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 00:51:04 -0500

    Update file 4/9/2024, 12:51:03 AM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,21 +1,11 @@
+            left= left.next;
+            lp++;
         }
-
-        //move/resize window
-        while(right != null){
-            right=right.next;
-            rp++;
-            //while(rp-lp>n){
-                left=left.next;
-                lp++;
-            //}
-        }
-        //remove lp or left node
-        while(cp<lp-1){
+        while(cp < lp-1){
             curr=curr.next;
             cp++;
         }
-        curr.next=left.next;
-
+        curr.next = left.next;
         return head;
     }
 }
