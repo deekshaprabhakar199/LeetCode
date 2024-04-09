bdab0f3d4d39bc2fe95f48f5f2974a985e73cc5e
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-08 19:37:02 -0500

    Update file 4/8/2024, 7:37:02 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.java
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -1,18 +1,14 @@
-        }
-            }
-                r = mid-1;
-            else{
+            else if(matrix[mid][cols-1] > target){
             }
                 l = mid+1;
-            else if(target> matrix[mid][cols-1]){
             }
+            else{
                 break;
-                finalRow = mid;
+                r = mid-1;
+            }
+        }
         int l2=0, r2= cols-1;
         while(l2<=r2){
             int mid= l2+(r2-l2)/2;
-            if(matrix[finalRow][mid]==target){
-                return true;
-            }
-            else if(matrix[finalRow][mid]> target){
+                finalRow = mid;
 [
