ef7b6e08783e781071fc67aa2ce1781f77bdf78f
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 14:21:21 -0500

    Create file

---
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,18 @@
+        }
+        else if(min>root.val){
+            ans =lowestCommonAncestor(root.right,p,q);
+        }
+        else{
+            //TreeNode left= lowestCommonAncestor(root.left,p,q);
+            //TreeNode right = lowestCommonAncestor(root.right,p,q);
+            //if(left==null) return right;
+            //else if(right==null) return left;
+            //else 
+        }
+        return ans;
+            return root;
+    }
+            ans= lowestCommonAncestor(root.left,p,q);
+        if(max<root.val){       //left tree has both
+
+[
