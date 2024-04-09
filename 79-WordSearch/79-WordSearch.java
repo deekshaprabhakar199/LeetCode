b2cde22059e0b77f81d5be8d2c72c053a30ced84
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 18:27:36 -0500

    Create file

---
+++ 79-WordSearch/79-WordSearch.java
@@ -0,0 +1,20 @@
+        if(i<0 || j<0 || i>=row || j>= col || index >= word.length() ||board[i][j] != word.charAt(index)){
+        int col = board[0].length;
+            return false;
+        }
+        if (index == word.length()-1){
+            return true;
+        }
+
+        char temp = board[i][j];
+    public boolean backtrack(char[][] board, int i, int j, String word, int index){
+        int row= board.length;
+
+    }
+            }
+        }
+        return false;
+                }
+        board[i][j] = '#';
+
+[
