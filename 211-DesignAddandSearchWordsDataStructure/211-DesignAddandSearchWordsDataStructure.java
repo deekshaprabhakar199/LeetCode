6889e9909c6563b3e53f8d6ee2f53eeb7de2cfcd
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 10:39:47 -0500

    Update file 4/9/2024, 10:39:47 AM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.java
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.java
@@ -1,21 +1,16 @@
         if( c== '.'){
+        char c= word.charAt(index);
             for(TrieNode val: curr.child.values()){
+                if(searchHelper(word, val, index+1)) return true;
+            } 
+            return false;
         }
         else{
             return curr.child.get(c) != null && searchHelper(word,curr.child.get(c), index+1);
         }
-                return searchHelper(word, val, index+1);
-            }
-        return false;
-        char c= word.charAt(index);
+        //return false;
     }
 }
-        }
-            return curr.isWord;
-        if(index == word.length()){
-    public boolean searchHelper(String word, TrieNode curr, int index){
-    }
-        return searchHelper(word,root, 0);
-    public boolean search(String word) {
-    
+
+/**
 [
