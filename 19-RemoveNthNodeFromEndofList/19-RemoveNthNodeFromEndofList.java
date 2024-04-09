3f1e16aeabafa3898b97b760a2e2affa36233108
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 00:59:20 -0500

    Update file 4/9/2024, 12:59:19 AM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.java
@@ -1,12 +1,23 @@
+        for(int i=0; i<n; i++){
+        ListNode right = head;
+        ListNode curr= head;
+        ListNode left= head;
+        int lp=0, cp=0, rp=0;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+class Solution {
+ */
+ * }
+            if(right.next == null) return head.next;
+            right = right.next;
+            rp++;
+        }
+        while(right != null){
+            right = right.next;
+            rp++;
             left= left.next;
             lp++;
         }
         while(cp < lp-1){
             curr=curr.next;
             cp++;
-        }
-        curr.next = left.next;
-        return head;
-    }
-}
 [
