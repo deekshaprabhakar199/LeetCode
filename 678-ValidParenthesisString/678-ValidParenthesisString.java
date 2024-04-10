d7dc251c3e69394bf0d5aec4d4b8ea85df3fc0f8
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-10 08:11:54 -0500

    Create file

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,21 @@
+                    }
+                    else{
+                        return false;
+                    }
+                    break;
+            }
+        }
+        while(!leftstack.isEmpty() && !starstack.isEmpty()){
+            if(leftstack.pop()>starstack.pop()){
+                return false;
+            }
+        }
+        return leftstack.isEmpty();
+                        starstack.pop();
+                    else if(!starstack.isEmpty()){
+                    }
+                        leftstack.pop();
+    }
+        //return true;
+}
+"()"
