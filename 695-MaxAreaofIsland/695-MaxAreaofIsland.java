c29a5346e9acb88e930e09eef8eb0878ebb26d16
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 19:39:54 -0500

    Create file

---
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.java
@@ -0,0 +1,19 @@
+    public int dfs(int[][] grid, int i, int j){
+        if(i<0 || j<0 || i>= grid.length || j>= grid[0].length || grid[i][j]==0){
+
+    }
+        return max;
+        }
+            }
+                }
+                    max = Math.max(max, area);
+                    area = dfs(grid,i,j);
+                if(grid[i][j]==1){
+            for( int j=0; j<cols; j++){
+        for( int i=0; i<rows; i++){
+
+        int area=0, max=0;
+        int cols = grid[0].length;
+        int rows= grid.length;
+    public int maxAreaOfIsland(int[][] grid) {
+[
