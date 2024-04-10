0b09b665c4f49674865da4566ef6cd644aa99221
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 19:31:41 -0500

    Create file

---
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -0,0 +1,16 @@
+        }
+            }
+                }
+                    dfs(grid, i, j);
+                    islandCount++;
+                if(grid[i][j]=='1'){
+            for( int j=0; j< cols; j++){
+        for(int i=0; i<row; i++){
+
+        int islandCount = 0;
+        int cols = grid[0].length;
+        return islandCount;
+    }
+        int row= grid.length;
+    public int numIslands(char[][] grid) {
+[
