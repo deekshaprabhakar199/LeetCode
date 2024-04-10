2e550818413e54b9ee0a1dcef974b628e3ec825e
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-10 00:46:16 -0500

    Update file 4/10/2024, 12:46:16 AM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,21 +1,18 @@
-        
-        // Check edge case
-        if (amount < 1) return 0;
+        if(amount<1) return 0;
+    public int coinChange(int[] coins, int amount) {
+        int[] money = new int[amount+1];
 
-        // Create DP array
-        int[] minCoinsDP = new int[amount + 1];
+        for( int i=1; i<= amount; i++){
+            money[i] = Integer.MAX_VALUE;
+        }
 
-        for (int i = 1; i <= amount; i++) {
-
-            minCoinsDP[i] = Integer.MAX_VALUE;
-
-            // Try each coin
-            for (int coin : coins) {
-                if (coin <= i && minCoinsDP[i - coin] != Integer.MAX_VALUE)
-                    minCoinsDP[i] = Math.min(minCoinsDP[i], 1 + minCoinsDP[i - coin]);
-                }
+            for( int coin:coins){
+                if(coin <= i && money[i-coin] != Integer.MAX_VALUE){
             }
-
-        return minCoinsDP[amount] == Integer.MAX_VALUE ? -1 : minCoinsDP[amount];
+                    money[i] = Math.min(money[i], 1+money[i-coin]);
+                }
     }
+        return money[amount] == Integer.MAX_VALUE ? -1 : money[amount];
+}
+class Solution {
 [
