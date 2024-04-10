3a9bcad60bf0071efc49c236a443cf96ef406446
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-10 08:19:48 -0500

    Update file 4/10/2024, 8:19:47 AM

--- 678-ValidParenthesisString/678-ValidParenthesisString.java
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -1,21 +1,20 @@
-                    }
-                    else{
-                        return false;
-                    }
-                    break;
-            }
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(Comparator.comparingInt(p -> p.distance));
+        for(int[] temp : points)
+            maxHeap.offer(new Pair(findDistance(temp[0] , temp[1]), temp[0], temp[1]));
+        int ans[][] = new int[k][2];
+        int idx = 0;
+        while(idx < k){
+            ans[idx][0] = maxHeap.peek().x;
+            ans[idx][1] = maxHeap.peek().y;
+            maxHeap.poll();
+            idx++;
         }
-        while(!leftstack.isEmpty() && !starstack.isEmpty()){
-            if(leftstack.pop()>starstack.pop()){
-                return false;
-            }
-        }
-        return leftstack.isEmpty();
-                        starstack.pop();
-                    else if(!starstack.isEmpty()){
-                    }
-                        leftstack.pop();
+        return ans;
     }
-        //return true;
+    public int findDistance(int x,int y){
+        return (x * x) + (y * y);
+    }
 }
-"()"
+[
