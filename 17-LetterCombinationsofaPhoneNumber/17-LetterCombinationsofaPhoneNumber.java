cdb921e7347ee1251559253441a87013dced725a
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-04-09 19:08:57 -0500

    Update file 4/9/2024, 7:08:57 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,2 +1,14 @@
-class Solution {
+}
+
+
+// class Solution {
+//     List<String> result;
+//     Map<Character, String> numMap;
+
+//     public List<String> letterCombinations(String digits) {
+//         result = new LinkedList<>();
+//         if(digits == null || digits.length() == 0) return result;
+
+//         numMap = new HashMap<>();
+//         numMap.put('2', "abc");
 "
