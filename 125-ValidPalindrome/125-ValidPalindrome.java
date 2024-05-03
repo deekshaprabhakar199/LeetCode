72bfcb375cbf2616799c6e7d78b6ddf17c3a3339
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-05-03 01:53:44 -0500

    Update file 5/3/2024, 1:53:44 AM

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -1,16 +1,15 @@
-                }
-                l++;
+        }
+                if(str[l] != str[r]) return false;
             }
-            else if(!isAlphanumeric(str[r])) r--;
-            else {
-                if(str[l] != str[r]) {
-                    return false;
-        while(l<=r){
-            if(!isAlphanumeric(str[l])){
                 l++;
                 r--;
-            }
-        }
-        return true;
-    }
+            else{
+            else if(!isAlphanumeric(str[r])) r--;
+            if(!isAlphanumeric(str[l])) l++;
+        while(l<=r){
+        int l=0, r= str.length-1;
+        char[] str = s.toCharArray();
+    public boolean isPalindrome(String s) {
+class Solution {
+        s=s.toLowerCase();
 "
