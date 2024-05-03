51fb5d300c31cc6457b654ca2d5dcc9c87c85e74
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-05-03 01:57:36 -0500

    Update file 5/3/2024, 1:57:36 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,13 +1,20 @@
-class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int l=0, r=numbers.length-1;
+        return new int[]{-1,-1};
+        int l=0, r= numbers.length-1;
         while(l<r){
             sum = numbers[l] + numbers[r];
-        int sum=0;
+        }
             if(sum>target){
                 r--;
             }
-            else if(sum <target){
+            else if(sum< target){
                 l++;
             }
+            else{
+                return new int[]{l+1,r+1};
+            }
+    }
+}
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int sum=0;
 [
