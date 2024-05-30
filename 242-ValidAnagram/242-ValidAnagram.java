cd44468d0660dd85945bd2dae940da304a0db5f1
Deeksha Prabhakar <36487877+deekshaprabhakar199@users.noreply.github.com>
2024-05-30 17:38:35 -0500

    Update file 5/30/2024, 5:38:35 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> anagram = new HashMap<>();
-        for( char c: s.toCharArray()){
-            anagram.put(c, anagram.getOrDefault(c,0)+1);
+        for(Character c: s.toCharArray()){
+            anagram.put(c,anagram.getOrDefault(c,0)+1);
+        }
+        for(Character c: t.toCharArray()){
         }
     }
-        for(char c: t.toCharArray()){
-            anagram.put(c, anagram.getOrDefault(c,0)-1);
-        }
+            anagram.put(c,anagram.getOrDefault(c,0)-1);
             if(anagram.get(c)==0) anagram.remove(c);
         return anagram.isEmpty();
 }
